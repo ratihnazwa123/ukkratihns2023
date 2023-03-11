67063147a80329704292d4ec89b1a5c53b61b522
ratihnazwa123 <127474054+ratihnazwa123@users.noreply.github.com>
2023-03-11 11:25:08 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,37 @@
+<div class="container"> 
+    <h4 class="text-center mt-3">Uji Kompetensi Keahlian <br>Rekayasa Perangkat Lunak (RPL) <br> Tahun 2022/2023</h4>
+    <hr>
+    <div class="row mt-3">
+    <div class="col-md-8">
+        <div class="card">
+        <div class="card-header" style="background-color: #87CEFA">INFORMASI</div>
+        <div class="card-body">
+            Aplikasi Pelaporan Pengaduan Masyarakat ini dibuat untuk memenuhi Tugas Uji Kompetensi Keahlian RPL Tahun 2023<br><br>
+            Petunjuk Penggunaan Aplikasi :
+
+            <br>1. Daftar Akun terlebih dahulu, Jika tidak mempunyai akun, Jika sudah mempunyai akun klik <b>Login</b>.
+            <br>2. Jika sudah login, Masyarakat bisa mengisi Laporan tentang form pengaduan.
+            <br>3. Setelah selesai mengisi form, Masyarakat sudah bisa klik <b>"Kirim"</b>
+            <br>4. Selesai mengirim form, Masyarakat akan menunggu tanggapan dari Petugas.
+            <br>5. Riwayat dari pengaduan masyarakat akan diterima oleh Petugas.
+            <br>6. Jika sudah selesai akan berbentuk <b>"Selesai"</b>, Jika ditolak akan berbentuk <b>"Menunggu"</b>
+        </div>
+        </div>
+        <div class="card-footer" style="background-color: #87CEFAdiv"></div>
+        </div>
+    </div>
+    <div class="col-md-4">
+    <div class="card">
+        <div class="card-header" style="background-color: #87CEFA">KONTAK</div>
+        <div class="card-body">
+           Dikembangkan oleh <br>
+           Nama : Ratih Nazwa Sopian<br>
+           Kelas : XII RPL 2<br>
+           No.Hp: +62882-0003-47749<br>
+           Instagram: naz.wra<br>
+</div>
+        <div class="card-footer" style="background-color: #87CEFA"></div>
+        </div>
+    </div>
+    </div>
+</div>
---
+++ layouts/footer.php
@@ -0,0 +1,10 @@
+<footer class="fixed - buttom" style="background-color: #87CEFA">
+  <div class="container">
+    <p class="text-center">UKK RPL 2023 | Ratih | SMK MAHARDIKA</p>
+  </div>
+
+</footer>
+
+<script type="text/javascript" src="../assets/js/bootstrap.min.js"></script>
+</body>
+</html>
